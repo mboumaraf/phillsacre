12cbaf0e670a8c7576b39dd35860cabe7638ca06
phillip.sacre <phillip.sacre@5f9f4c0c-98fe-11dd-ad9f-f77439c74b79>
2008-10-13 11:54:33 +0000

    Removed logs

--- mail/trunk/application/logs/2008-10-13.log.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2008-10-13 09:15:29 +01:00 --- error: Uncaught Kohana_404_Exception: The page you requested, welcome, could not be found. in file C:/wamp/www/mail/system/core/Kohana.php on line 816
